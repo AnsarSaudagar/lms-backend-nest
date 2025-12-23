8208be845ac4ab4b734d89a65bea93cb735cde31
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-23 19:05:42 +0530

    installed argon2 and created helper functions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  async hashPassword(password: string): Promise<string> {
+    const hash: string = await argon2.hash(password);
+    return hash;
+  }
+
+  async verifyPassword(hash: string, password: string): Promise<boolean> {
+    const isValid: boolean = await argon2.verify(hash, password);
+    return isValid;
+  }
+}
