0462745c9a67bb437ce705c53410331e4b509d30
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-24 10:38:22 +0530

    jwt modules

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,8 +2,15 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from '../users/users.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: 'SECRET_KEY',
+      signOptions: { expiresIn: '15m' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UsersService],
 })
