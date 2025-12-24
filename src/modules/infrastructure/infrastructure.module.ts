032e74e087cfad3f808a8b50087220926bba012c
Ansar Hussain Saudagar <ansarsaudagar40@gmail.com>
2025-12-24 17:40:47 +0530

    added cache manager and redis

--- src/modules/infrastructure/infrastructure.module.ts
+++ src/modules/infrastructure/infrastructure.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ErrorLoggerModule } from './error-logger/error-logger.module';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-store';
 
 @Module({
-  imports: [ErrorLoggerModule],
-  exports: [ErrorLoggerModule]
+  imports: [ErrorLoggerModule,
+    CacheModule.registerAsync({
+      isGlobal: true,
+      useFactory: () => ({
+        stores: redisStore,
+        host: process.env.REDIS_HOST,
+        port: process.env.REDIS_PORT,
+        ttl: 60, // seconds
+      })
+    })
+  ],
+  exports: [ErrorLoggerModule],
 })
 export class InfrastructureModule {}
